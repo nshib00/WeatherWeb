e37aa1f2622a75ba74607e6c7d4980e92e70949e
Nikita Shibaev <nshib.54321@gmail.com>
2025-05-30 22:22:03 +0300

    Add home page view and template, create city form

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from weather.forms import CityForm
+from .services.weather_api import WeatherAPIService
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    weather_data = None
+    error = None
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data['city']
+            weather_data = WeatherAPIService.get_today_weather(city)
+            if not weather_data:
+                error = "Город не найден." 
+    else:
+        form = CityForm()
+
+    context = {
+        'form': form,
+        'weather_data': weather_data, 
+        'error': error,
+        'title': 'Погода на сегодня'
+    }
+    return render(request, 'weather/home.html', context)

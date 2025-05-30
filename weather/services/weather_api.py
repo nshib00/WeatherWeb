e356a445659ad3e96c7a937570f61f7b0c524f3e
Nikita Shibaev <nshib.54321@gmail.com>
2025-05-30 22:20:37 +0300

    Move and rename weather API service

--- weather/services/weather_api.py
+++ weather/services/weather_api.py
@@ -7,7 +7,7 @@ from datetime import datetime
 load_dotenv(find_dotenv())
 
 
-class APIService:
+class WeatherAPIService:
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
     query_params = {
         "unitGroup": "metric",
@@ -18,10 +18,10 @@ class APIService:
     @classmethod
     def __build_query(cls, query: str) -> str:
         query += "?"
-        for i, param in enumerate(APIService.query_params.items()):
+        for i, param in enumerate(cls.query_params.items()):
             p_name, p_value = param
 
-            if i < len(APIService.query_params) - 1:
+            if i < len(cls.query_params) - 1:
                 query += f"{p_name}={p_value}&"
             else:
                 query += f"{p_name}={p_value}"
@@ -31,10 +31,11 @@ class APIService:
     @classmethod
     def get_today_weather(cls, city_query: str):
         today = datetime.now().strftime(r"%Y-%m-%d")
-        request_url = APIService.__build_query(f"{APIService.url}/{city_query}/{today}")
+        request_url = cls.__build_query(f"{cls.url}/{city_query}/{today}")
 
         with HTMLSession() as session:
             response = session.get(request_url)
+            print(request_url)
 
         return response.json()
 

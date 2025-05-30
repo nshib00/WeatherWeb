6578527794a78ec49e099ea97d8a492c136d7c90
Nikita Shibaev <nshib.54321@gmail.com>
2025-05-30 22:21:09 +0300

    Create city model

--- weather/models.py
+++ weather/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from users.models import User
 
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    favorite = models.BooleanField(default=False)
+    searched_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

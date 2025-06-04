dfc4cc347294ca1c262a3ad54bfc77d87554efc0
Nikita Shibaev <nshib.54321@gmail.com>
2025-06-04 11:17:14 +0300

    Update city model, apply migrations

--- weather/models.py
+++ weather/models.py
@@ -4,11 +4,10 @@ from users.models import User
 
 class City(models.Model):
     name = models.CharField(max_length=100)
-    region = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
+    region = models.CharField(max_length=255)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    favorite = models.BooleanField(default=False)
+    is_default = models.BooleanField(default=False)
     searched_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
